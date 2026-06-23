e16888017dbb11a1595ebb7d550afe04b6a5e5f5
Tushar Mangla <tushar.mangla1120@gmail.com>
2026-06-23 13:10:05 +0530

    refactor: remove AnnouncementBanner component and comment out its usage in SiteHeader

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -1,13 +1,13 @@
 'use client';
 
-import AnnouncementBanner from './AnnouncementBanner';
 import Navbar from './Navbar';
 
 export default function SiteHeader() {
     return (
         <div className="sticky top-0 z-[100] w-full">
-            <AnnouncementBanner />
+            {/* AnnouncementBanner hidden */}
             <Navbar />
         </div>
     );
 }
+
